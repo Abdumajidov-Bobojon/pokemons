4baa6a39a0206f049b95d4ad11eb5757c90af42d
yakohere <yakhyoismoildjonov@gmail.com>
2021-11-16 00:40:48 +0500

    fetching all pokemons with every data needed

--- src/containers/PokemonList.tsx
+++ src/containers/PokemonList.tsx
@@ -2,12 +2,34 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import api from "../utils/axios";
 
+interface PokemonData {
+    name: string;
+    id: string,
+    image: string,
+    type: string
+}
+
 const PokemonList: Function = () => {
-    const [pokemons, setPokemons] = useState()
+    const [pokemons, setPokemons] = useState<PokemonData[] | null>(null)
 
     useEffect(() => {
-        api.get("/pokemon?limit=100").then(res => console.log(res))
+        let promises = []
+        for (let i = 1; i <= 100; i++) {
+            let url = `https://pokeapi.co/api/v2/pokemon/${i}`
+            promises.push(api.get(url).then(res => res.data))
+        }
+
+        Promise.all(promises).then(res => {
+            setPokemons(res.map(e => ({
+                name: e.name,
+                id: e.id,
+                image: e.sprites['front_default'],
+                type: e.types.map((type: any) => type.type.name).join(", ")
+            })));
+        })
     }, [])
+
+    console.log(pokemons)
 
     return (
         <Wrapper>
